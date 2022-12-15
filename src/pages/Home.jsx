6744177483422acec2d3e6c2e65b43c11fb17f2d
ilyalonkin <Lonkin555@mail.ru>
2022-12-15 17:23:09 +0300

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -110,7 +110,7 @@ export const Home = () => {
                 title={obj.title}
                 imageUrl={obj.imageUrl ? obj.imageUrl : ""}
                 user={obj.author}
-                createdAt={obj.createdAt}
+                createdAt={obj.createdAt.toLocaleDateString("en-US")}
                 viewsCount={obj.viewsCount}
                 commentsCount={obj.commentsCount}
                 tags={obj.tags}
